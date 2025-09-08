e1770086dc43f8263ae099ddaf5a3c54bd48d02a
s93frost <47425976+s93frost@users.noreply.github.com>
2025-09-08 16:33:38 +0100

    made two versions - If you want full MTL/texture support: Use VTK’s vtkOBJImporter for OBJ files. (Viewer / main) If you want mesh manipulation/analysis: Keep using trimesh. (Viewer_alt / main_alt)

---
+++ main_alt.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QApplication
+from viewer_alt import ViewerApp
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ViewerApp()
+    window.show()
+    sys.exit(app.exec_())
